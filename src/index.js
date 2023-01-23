b8a84146775927630a13b72f3b564305a6c147d1
Ozgur Altug <ozguraltug06@gmail.com>
2023-01-23 22:22:09 +0300

    v1

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
-import React from 'react';
+import React, { StrictMode } from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/scss/bootstrap.scss";
+// import "bootstrap"; Bu import eğer bootstrap static olarak kullanılacaksa Javascript i etkinleştirmek için kullanılmalıdır. Dinamik yapıda buna gerek yoktur.
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <App/>
 );
 
 // If you want to start measuring performance in your app, pass a function
